14f0974528af248e780bfc8c0491bab0070b4cc6
faysor <faysorx3@gmail.com>
2020-04-03 12:00:55 -0400

    feat: ruta de la barra para el debug

--- inventory/urls.py
+++ inventory/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.urls import include, path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
+import debug_toolbar
 
 urlpatterns = [
     path('api/token',
@@ -34,3 +36,11 @@ urlpatterns = [
         include('inventory.common.urls'),
         name='api_1.0'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/',
+            include(debug_toolbar.urls),
+            name='debug_toolbar'),
+    ] + urlpatterns
